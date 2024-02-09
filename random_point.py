a543ef54deda7e127cd09c936cd19d83c797f83f
alvine17 <103496992+alvine17@users.noreply.github.com>
2024-02-09 16:24:51 +0100

    change

--- random_point.py
+++ random_point.py
@@ -2,12 +2,12 @@ import pygame
 import random  
 
 class Point:
-    def __init__(self, screen_width, screen_height):
-        self.screen_width = screen_width
-        self.screen_height = screen_height
+    def __init__(self, s_width, s_height):
+        self.s_width = s_width
+        self.s_height = s_height
         self.radius = 7
         self.color = (0, 255, 0)  
-        self.position = pygame.Vector2(random.randint(0, self.screen_width), random.randint(0, self.screen_height))
+        self.position = pygame.Vector2(random.randint(0, self.s_width), random.randint(0, self.s_height))
 
     def draw(self, screen):
         pygame.draw.circle(screen, self.color, (int(self.position.x), int(self.position.y)), self.radius)
